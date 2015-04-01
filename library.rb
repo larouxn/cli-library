33d190d84de4350f8f5eb43b580de2cf0809872c
larouxn <larouxn@gmail.com>
2015-04-01 00:10:27 -0400

    Finished intial regex implementation

--- library.rb
+++ library.rb
@@ -6,13 +6,13 @@ def help
   Usage: ruby library.rb
 
   Commands:
-  - **add "$title" "$author"**: adds a book to the library with the given title and author. All books are unread by default.
-  - **read "$title"**: marks a given book as read.
-  - **show all**: displays all of the books in the library
-  - **show unread**: display all of the books that are unread
-  - **show all by "$author"**: shows all of the books in the library by the given author.
-  - **show unread by "$author"**: shows the unread books in the library by the given author
-  - **quit**: quits the program.
+  - add "title" "author"      Adds a book to the library with the given title and author. All books are unread by default.
+  - read "title"              Marks a given book as read.
+  - show all                  Displays all of the books in the library.
+  - show unread               Display all of the books that are unread.
+  - show all by "author"      Shows all of the books in the library by the given author.
+  - show unread by "author"   Shows the unread books in the library by the given author.
+  - quit                      Quits the program.
   help
 end
 
@@ -22,20 +22,27 @@ def add(title, author)
 end
 
 def read(title)
-  puts "#{title} marked as read"
+  puts "Marked #{title} as read"
   # semi-persistent data code
 end
 
-def show (all_or_unread, by, author)
+def show (all_or_unread, author = nil)
   # multiple optional arguments, case based on what the arguments are
-  case all_or_unread
-  when "all"
-    # beep
-  when "unread"
-    # boop
+  if author == nil
+    puts "Here's #{all_or_unread}"
   else
-    puts "Improper arguments"
+    puts "Here's #{all_or_unread} by #{author}"
   end
+
+
+  # case all_or_unread
+  # when "all"
+  #   # beep
+  # when "unread"
+  #   # boop
+  # else
+  #   puts "Improper arguments"
+  # end
   # semi-persistent data code
 end
 
@@ -62,10 +69,16 @@ begin
     when /^add "{1}.*?"{1} "{1}.*?"{1}$/
       line = line.scan(/"{1}.*?"{1}/)
       add(line[0], line[1])
-    when /^read .+$/
-      puts "WILL BE USED TO SET READ FLAGS"
-    when /^show .+$/
-      puts "WILL BE USED TO LIST BOOKS"
+    when /^read "{1}.*?"{1}$/
+      line = line.scan(/"{1}.*?"{1}/)
+      read(line[0])
+    when /^show (all|unread)\s?(by "{1}.*?"{1})?$/
+      line = line.scan(/(all|unread)|("{1}.*?"{1})/)
+      if line[1] == nil # basic
+        show(line[0][0])
+      else # with author
+        show(line[0][0], line[1][1])
+      end
     when /^(quit|exit)$/
       quit
     when ""
