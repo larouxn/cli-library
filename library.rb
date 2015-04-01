58a678da3645bf5ed506ba44e22fb981aca692dd
larouxn <larouxn@gmail.com>
2015-03-31 21:40:20 -0400

    Added regex for commands

--- library.rb
+++ library.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
-
-library = File.new("library.txt", "w+")
+require 'tempfile'
 
 def help
   puts <<-help
@@ -14,9 +13,7 @@ def help
   - **show all by "$author"**: shows all of the books in the library by the given author.
   - **show unread by "$author"**: shows the unread books in the library by the given author
   - **quit**: quits the program.
-
   help
-  exit
 end
 
 def add
@@ -29,6 +26,54 @@ def show
 end
 
 def quit
+  exit
+end
+
+library = Tempfile.new("library.txt")
+
+begin
+  loop do
+    print ">> "
+
+    begin
+      line = $stdin.gets.chomp
+    rescue NoMethodError, Interrupt
+      exit
+    end
+
+    case line
+    when /help/
+      help
+    when /^add .+ .+/
+      puts "WILL BE USED TO ADD BOOKS"
+    when /^read .+/
+      puts "WILL BE USED TO SET READ FLAGS"
+    when /^show all/
+      puts "WILL BE USED TO LIST BOOKS"
+    when /^show unread/
+      puts "WILL BE USED TO LIST BOOKS"
+    when /^show all by .+/
+      puts "WILL BE USED TO LIST BOOKS"
+    when /^show unread by .+/
+      puts "WILL BE USED TO LIST BOOKS"
+    when /quit/
+      quit
+    when ""
+    else
+      puts "Command not recognized, please use 'help' for a list of available commands"
+    end
+
+    # if from_stdin
+    #   run = "echo \"%s\" | #{command}" % [ line, nil ]
+    # else
+    #   run = "#{command} %s" % [ line, nil ]
+    # end
+    # puts "$ #{run}" if debug
+    # system run
+    # warn "Use Ctrl-D (i.e. EOF) to exit" if line =~ /^(exit|quit)$/
+  end
+ensure
+  puts ""
   library.close
-  # execute ctrl-c, ctrl-d, exit command
+  library.unlink
 end
